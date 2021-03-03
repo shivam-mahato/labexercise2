80865150166ad6366527e9a823820acd34c42a22
SHIVAM <shivam-mahato>
2021-03-03 13:27:19 +0545

    initial commit

---
+++ lab question/qn_1.py
@@ -0,0 +1,6 @@
+# 1. Write a program that takes three number and prints their sum. Every number is given on a seprate line.
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=int(input("Enter the third number:"))
+sum=a+b+c
+print("the sum of all given num7ber is ",sum)
